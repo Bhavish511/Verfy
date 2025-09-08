d1be7ae53fe0832f3ae3807048dcade3e28f4f8f
Bhavish511 <Bhavishsindhi3@gmail.com>
2025-09-08 15:27:03 +0500

    change the configuration for vercel

--- api/index.js
+++
@@ -1,3 +0,0 @@
-// api/index.js
-const { handler } = require('../dist/serverless');
-module.exports = handler;
--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 import serverless from 'serverless-http';
-import bodyParser from 'body-parser';
 
 let cachedServer: any;
 
 async function bootstrapServer() {
   const expressApp = express();
-
-  // Use body-parser ONLY in local dev (not on Vercel)
-  if (!process.env.VERCEL) {
-    expressApp.use(bodyParser.json({ limit: '1mb' }));
-    expressApp.use(bodyParser.urlencoded({ extended: true }));
-  }
-
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors();
   await app.init();
@@ -23,7 +15,7 @@ async function bootstrapServer() {
   return serverless(expressApp);
 }
 
-export async function handler(req: any, res: any) {
+export default async function handler(req: any, res: any) {
   if (!cachedServer) {
     cachedServer = await bootstrapServer();
   }
